e96567d2c43d4c846a855ee4b02ba0d5af1f94f2
mtodorov95 <mtodorov95@gmail.com>
2020-08-23 10:30:41 +0300

    frontend init

---
+++ src/components/EditExercise.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EditExercise() {
+  return <div>Editing</div>;
+}
+
+export default EditExercise;
---
+++ src/components/ExercisesList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ExercisesList() {
+  return <div>Exercise list</div>;
+}
+
+export default ExercisesList;
